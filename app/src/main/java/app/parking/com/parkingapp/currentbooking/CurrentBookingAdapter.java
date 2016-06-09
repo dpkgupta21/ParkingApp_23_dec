f66e0e978a94017374b232ae0c412ba74643f74e
dpkgupta21 <dpk.gupta21@gmail.com>
2016-06-09 23:07:45 +0530

    changed ip

--- app/src/main/java/app/parking/com/parkingapp/currentbooking/CurrentBookingAdapter.java
+++ app/src/main/java/app/parking/com/parkingapp/currentbooking/CurrentBookingAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import java.util.List;
 
 import app.parking.com.parkingapp.R;
-import app.parking.com.parkingapp.model.CreateOrderResponseDTO;
+import app.parking.com.parkingapp.model.OrderHistoryDTO;
 import app.parking.com.parkingapp.utils.HelpMe;
 
 /**
@@ -19,9 +19,9 @@ import app.parking.com.parkingapp.utils.HelpMe;
 public class CurrentBookingAdapter extends BaseAdapter {
 
     private Context context;
-    private List<CreateOrderResponseDTO> list;
+    private List<OrderHistoryDTO> list;
 
-    public CurrentBookingAdapter(Context context, List<CreateOrderResponseDTO> list) {
+    public CurrentBookingAdapter(Context context, List<OrderHistoryDTO> list) {
         this.context = context;
         this.list = list;
     }
@@ -54,32 +54,26 @@ public class CurrentBookingAdapter extends BaseAdapter {
             holder = (CurrentBookingViewHolder) convertView.getTag();
         }
 
-        CreateOrderResponseDTO responseDTO = list.get(position);
-        holder.plateNumber.setText(responseDTO.getVehicleInfo().getPlateNo());
-        holder.pickDate.setText(HelpMe.getFlightDisplayDateTime(responseDTO
-                .getDriverInfo().getPickupDriverInfo()
-                .getPickUpTime()));
-        holder.dropDate.setText(HelpMe.getFlightDisplayDateTime(responseDTO.getDriverInfo()
-                .getDropoffDriverInfo().getDropOffTime()));
-        holder.vehicleModel.setText(responseDTO.getVehicleInfo().getVehicleModel());
-        holder.orderStatus.setText(responseDTO.getOrderStatus().getStatus());
+        OrderHistoryDTO responseDTO = list.get(position);
+        holder.orderNumber.setText(responseDTO.getOrderNo());
+        holder.slotNumber.setText(responseDTO.getSlotNo());
+        holder.creationDate.setText(HelpMe.getFlightDisplayDateTime(responseDTO.getCreationDate()));
+        holder.orderStatus.setText(responseDTO.getOrderStatus());
 
         return convertView;
     }
 
     public class CurrentBookingViewHolder {
-        TextView plateNumber;
-        TextView pickDate;
-        TextView dropDate;
+        TextView orderNumber;
+        TextView slotNumber;
+        TextView creationDate;
         TextView orderStatus;
-        TextView vehicleModel;
 
         CurrentBookingViewHolder(View view) {
-            plateNumber = (TextView) view.findViewById(R.id.plate_number_value);
-            pickDate = (TextView) view.findViewById(R.id.pick_value);
-            dropDate = (TextView) view.findViewById(R.id.drop_date_value);
+            orderNumber = (TextView) view.findViewById(R.id.order_number_value);
+            slotNumber = (TextView) view.findViewById(R.id.slot_value);
+            creationDate = (TextView) view.findViewById(R.id.creationd_date_value);
             orderStatus = (TextView) view.findViewById(R.id.order_status_value);
-            vehicleModel = (TextView) view.findViewById(R.id.vehicle_name_value);
         }
     }
 }
--- app/src/main/java/app/parking/com/parkingapp/currentbooking/CurrentBookingFragment.java
+++ app/src/main/java/app/parking/com/parkingapp/currentbooking/CurrentBookingFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,23 +17,17 @@ import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import app.parking.com.parkingapp.R;
 import app.parking.com.parkingapp.customViews.CustomProgressDialog;
 import app.parking.com.parkingapp.fragments.BaseFragment;
-import app.parking.com.parkingapp.model.CreateOrderResponseDTO;
-import app.parking.com.parkingapp.model.PurchaseOrderDTO;
-import app.parking.com.parkingapp.preferences.ParkingPreference;
-import app.parking.com.parkingapp.utils.AppConstants;
+import app.parking.com.parkingapp.model.OrderHistoryDTO;
 import app.parking.com.parkingapp.utils.AppUtils;
-import app.parking.com.parkingapp.webservices.handler.OrderStatusAPIHandler;
+import app.parking.com.parkingapp.webservices.handler.OrderHistoryAPIHandler;
 import app.parking.com.parkingapp.webservices.ihelper.WebAPIResponseListener;
 
 
@@ -42,21 +35,8 @@ public class CurrentBookingFragment extends BaseFragment {
 
     private View view;
     private Activity mActivity;
+    private ListView currentBookingListView;
 
-    private Toolbar mToolbar;
-    private TextView toolbar_title, toolbar_right_tv, no_service_tv;
-    private RelativeLayout toolbar_right_rl;
-    private ImageView payment;
-    private ListView currentBookingList;
-    private CreateOrderResponseDTO createOrderResponseDTO;
-    private PurchaseOrderDTO purchaseOrderDTO;
-    private boolean isFlight;
-    private boolean isVehicle;
-    private boolean isService;
-    private boolean isPayment;
-    private boolean isOrder;
-    private boolean isPickUpInfo;
-    private boolean isDropOffInfo;
 
     public static CurrentBookingFragment newInstance(String param1, String param2) {
         CurrentBookingFragment fragment = new CurrentBookingFragment();
@@ -83,29 +63,20 @@ public class CurrentBookingFragment extends BaseFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         CustomProgressDialog.showProgDialog(mActivity, null);
-        String param = ParkingPreference.getEmailId(mActivity);
-        Map<String, String> params = new HashMap<>();
-        params.put("email", param);
-        JSONObject jsonObject = new JSONObject(params);
-        String auth = ParkingPreference.getKeyAuthtoken(mActivity);
-        String userId = ParkingPreference.getUserid(mActivity);
-        new OrderStatusAPIHandler(mActivity, jsonObject.toString(), auth,
-                userId, manageOrderStatusResponse());
+        new OrderHistoryAPIHandler(mActivity, manageOrderStatusResponse());
     }
 
     private void initViews() {
 
-        mToolbar = (Toolbar) mActivity.findViewById(R.id.tool_bar);
-        toolbar_title = (TextView) mToolbar.findViewById(R.id.toolbar_title);
-        currentBookingList = (ListView) view.findViewById(R.id.currentBookingLIst);
+        Toolbar mToolbar = (Toolbar) mActivity.findViewById(R.id.tool_bar);
+        TextView toolbar_title = (TextView) mToolbar.findViewById(R.id.toolbar_title);
+        currentBookingListView = (ListView) view.findViewById(R.id.currentBookingLIst);
         //submit_button = (RelativeLayout) findViewById(R.id.submit_button);
 
         //mToolbar.setNavigationIcon(R.drawable.back_button);
-        toolbar_right_rl = (RelativeLayout) mToolbar.findViewById(R.id.toolbar_right_rl);
-        toolbar_right_tv = (TextView) mToolbar.findViewById(R.id.toolbar_right_tv);
+        RelativeLayout toolbar_right_rl = (RelativeLayout) mToolbar.findViewById(R.id.toolbar_right_rl);
         toolbar_title.setVisibility(View.VISIBLE);
         toolbar_title.setText(getResources().getString(R.string.order_history_title));
-
         toolbar_right_rl.setVisibility(View.INVISIBLE);
 
 
@@ -125,10 +96,10 @@ public class CurrentBookingFragment extends BaseFragment {
 //                    e.printStackTrace();
 //                }
                 try {
-                    Type type = new TypeToken<ArrayList<CreateOrderResponseDTO>>() {
+                    Type type = new TypeToken<ArrayList<OrderHistoryDTO>>() {
                     }.getType();
                     JSONArray array = new JSONArray(response);
-                    List<CreateOrderResponseDTO> createOrderResponseDTOs = new Gson()
+                    List<OrderHistoryDTO> createOrderResponseDTOs = new Gson()
                             .fromJson(array.toString(), type);
 
                     setListAdapter(createOrderResponseDTOs);
@@ -149,16 +120,16 @@ public class CurrentBookingFragment extends BaseFragment {
         return responseListener;
     }
 
-    private void setListAdapter(final List<CreateOrderResponseDTO> orderDTO) {
+    private void setListAdapter(final List<OrderHistoryDTO> orderDTO) {
         CurrentBookingAdapter adapter = new CurrentBookingAdapter(mActivity, orderDTO);
-        currentBookingList.setAdapter(adapter);
+        currentBookingListView.setAdapter(adapter);
 
-        currentBookingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        currentBookingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(mActivity, CurrentBookingDetails.class);
-                //intent.putExtra("orderno", orderDTO.get(position).getOrderNo());
-                intent.putExtra(AppConstants.ORDER_SUMMARY_KEY, orderDTO.get(position));
+                intent.putExtra("orderno", orderDTO.get(position).getOrderNo());
+                //intent.putExtra(AppConstants.ORDER_SUMMARY_KEY, createOrderResponseDTOs.get(position));
                 mActivity.startActivity(intent);
             }
         });
